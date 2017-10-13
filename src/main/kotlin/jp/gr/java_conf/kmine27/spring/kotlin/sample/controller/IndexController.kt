be510da8d726d3772fe0c4a4c659d4c98ac3cda1
kmine <k.minemura27@gmail.com>
2017-10-13 15:04:01 +0900

    for app engine

--- src/main/kotlin/jp/gr/java_conf/kmine27/spring/kotlin/sample/controller/IndexController.kt
+++ src/main/kotlin/jp/gr/java_conf/kmine27/spring/kotlin/sample/controller/IndexController.kt
@@ -2,9 +2,15 @@ package jp.gr.java_conf.kmine27.spring.kotlin.sample.controller
 
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
+import jp.gr.java_conf.kmine27.spring.kotlin.sample.domain.Person
 
 @RestController
 class IndexController {
 	@RequestMapping("/")
 	fun index(): String = "Hello world !"
+	@RequestMapping("/person")
+	fun person(): Person {
+		return Person()
+	}
+
 }
---
+++ src/main/kotlin/jp/gr/java_conf/kmine27/spring/kotlin/sample/domain/Person.kt
@@ -0,0 +1,6 @@
+package jp.gr.java_conf.kmine27.spring.kotlin.sample.domain
+
+class Person {
+	val name: String = "kmine"
+	val age: Int = 32
+}
